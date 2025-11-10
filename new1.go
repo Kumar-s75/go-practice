de9bdb2621785151f36375118df7a30bfd4eded7
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-11 01:00:15 +0530

    added new concept

--- new1.go
+++ new1.go
@@ -9,13 +9,25 @@
 // var variablename type = value
 
 // variablename := value
+// package main
+// import ("fmt")
+// func Main(){
+// 	var student1 string="john"
+// 	var student2 ="jae"
+// 	x:=2
+// 	fmt.Println(student1);
+// 	fmt.Println(student2);
+// 	fmt.Println(x);
+// }
+//  Variable Declaration Without Initial Value
 package main
 import ("fmt")
+
 func Main(){
-	var student1 string="john"
-	var student2 ="jae"
-	x:=2
-	fmt.Println(student1);
-	fmt.Println(student2);
-	fmt.Println(x);
+	var a string
+	var b int
+	var c bool
+ fmt.Println(a)
+ fmt.Println(b)
+ fmt.Println(c)
 }
