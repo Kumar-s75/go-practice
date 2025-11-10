610743437b01b9051d570ef0bc1b46915e58562a
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-11 01:49:35 +0530

    another example

--- new1.go
+++ new1.go
@@ -70,3 +70,18 @@ func main() {
   fmt.Println(c)
   fmt.Println(d)
 }
+
+package main
+import ("fmt")
+
+func main() {
+   var (
+     a int
+     b int = 1
+     c string = "hello"
+   )
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+}
