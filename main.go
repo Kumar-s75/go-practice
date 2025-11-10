226709c268d0c47e00e9b74ac9b7b48fef202958
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-11 00:55:29 +0530

    single type variable

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
-package main   
-import ("fmt")
+// package main   
+// import ("fmt")
 
-func main() {  
+// func main() {  
   
-("Hello World!")
-}
+// ("Hello World!")
+// }
+
+//gocompact code
+package main; import ("fmt"); func main() { fmt.Println("Hello World!");}
--- new1.go
+++ new1.go
@@ -1,7 +1,21 @@
+// package main
+// import ("fmt")
+
+// func main()
+// {
+//   fmt.Println("Hello World!")
+// }
+
+// var variablename type = value
+
+// variablename := value
 package main
 import ("fmt")
-
-func main()
-{
-  fmt.Println("Hello World!")
+func Main(){
+	var student1 string="john"
+	var student2 ="jae"
+	x:=2
+	fmt.Println(student1);
+	fmt.Println(student2);
+	fmt.Println(x);
 }
