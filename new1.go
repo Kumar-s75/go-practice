033bc2a6b8c1402467d5a7449b148b6004f08e27
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-12 19:23:16 +0530

    signed and unsigned integers

--- new1.go
+++ new1.go
@@ -128,3 +128,14 @@ func main() {
   fmt.Println(b3) // Returns false
   fmt.Println(b4) // Returns true
 }
+
+//signed and unsigned integers
+package main
+import ("fmt")
+
+func main(){
+	var x int=500
+	var y int=-4500
+	fmt.Printf("Type:%T,value:%v",x,x);
+	fmt.Printf("Type:%T,value:%v",y,y);
+}
