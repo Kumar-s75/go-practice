ddabd8fc6baea000f94eab9ced5b5c50c5ac0a96
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-12 21:36:07 +0530

    string

--- new1.go
+++ new1.go
@@ -154,3 +154,16 @@ func main(){
 	fmt.Printf("Type:%T,value:%v\n",txt2,txt2);
 	fmt.Printf("Type:%T,value:%v\n",txt3,txt3);
 }
+
+////string
+package main
+import ("fmt")
+func main(){
+	var txt1 string="Hello!"
+	var txt2 string
+	txt3:="World 1"
+
+	fmt.Printf("Type:%T,value:%v\n",txt1,txt1);
+	fmt.Printf("Type:%T,value:%v\n",txt2,txt2);
+	fmt.Printf("Type:%T,value:%v\n",txt3,txt3);
+}
