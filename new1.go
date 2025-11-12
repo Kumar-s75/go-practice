70a285db4002310b70dc4e6614b08898029c58e0
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-13 01:57:43 +0530

    learned slice

--- new1.go
+++ new1.go
@@ -259,3 +259,102 @@ func main(){
 	fmt.Printf("capacity = %d\n", cap(myslice2));
 
 }
+/////access element of a slice
+package main
+import  ("fmt")
+
+func main(){
+	prices := []int{10,20,30}
+
+	fmt.Println(prices[20])
+	fmt.Println(prices[0])
+	fmt.Println(prices[2])
+}
+
+/////change elements of a slice
+package main
+import ("fmt")
+
+func main(){
+	prices:=[]int{10,20,30}
+	prices[2]=50
+	fmt.Println(prices[0])
+	fmt.Println(prices[1])
+}
+
+///append elements to a slice
+////slice_name = append(slice_name, element1, element2, ...)
+package main
+import ("fmt")
+
+func main() {
+  myslice1 := []int{1, 2, 3, 4, 5, 6}
+  fmt.Printf("myslice1 = %v\n", myslice1)
+  fmt.Printf("length = %d\n", len(myslice1))
+  fmt.Printf("capacity = %d\n", cap(myslice1))
+
+  myslice1 = append(myslice1, 20, 21)
+  fmt.Printf("myslice1 = %v\n", myslice1)
+  fmt.Printf("length = %d\n", len(myslice1))
+  fmt.Printf("capacity = %d\n", cap(myslice1))
+}
+
+///append one slice to another slice
+// slice3 = append(slice1, slice2...)
+package main
+import("fmt")
+
+func main(){
+	myslice1 := []int{1,2,3}
+	myslice2 := []int{4,5,6}
+	myslice3 := append(myslice1,myslice2...)
+
+	fmt.Printf("myslice3=%v\n", myslice3)
+  fmt.Printf("length=%d\n", len(myslice3))
+  fmt.Printf("capacity=%d\n", cap(myslice3))
+
+}
+
+//chnage length of a slice
+package main
+import ("fmt")
+
+func main() {
+  arr1 := [6]int{9, 10, 11, 12, 13, 14} // An array
+  myslice1 := arr1[1:5] // Slice array
+  fmt.Printf("myslice1 = %v\n", myslice1)
+  fmt.Printf("length = %d\n", len(myslice1))
+  fmt.Printf("capacity = %d\n", cap(myslice1))
+
+  myslice1 = arr1[1:3] // Change length by re-slicing the array
+  fmt.Printf("myslice1 = %v\n", myslice1)
+  fmt.Printf("length = %d\n", len(myslice1))
+  fmt.Printf("capacity = %d\n", cap(myslice1))
+
+  myslice1 = append(myslice1, 20, 21, 22, 23) // Change length by appending items
+  fmt.Printf("myslice1 = %v\n", myslice1)
+  fmt.Printf("length = %d\n", len(myslice1))
+  fmt.Printf("capacity = %d\n", cap(myslice1))
+}
+
+//memory efficiency
+// copy(dest, src)
+package main
+import ("fmt")
+
+func main() {
+  numbers := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+  // Original slice
+  fmt.Printf("numbers = %v\n", numbers)
+  fmt.Printf("length = %d\n", len(numbers))
+  fmt.Printf("capacity = %d\n", cap(numbers))
+
+  // Create copy with only needed numbers
+  neededNumbers := numbers[:len(numbers)-10]
+  numbersCopy := make([]int, len(neededNumbers))
+  copy(numbersCopy, neededNumbers)
+
+  fmt.Printf("numbersCopy = %v\n", numbersCopy)
+  fmt.Printf("length = %d\n", len(numbersCopy))
+  fmt.Printf("capacity = %d\n", cap(numbersCopy))
+}
