de2f9de24258b029e57c78214be1516ba35dadd6
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-12 22:26:52 +0530

    creating a slice from make function

--- new1.go
+++ new1.go
@@ -208,6 +208,7 @@ func main() {
 }
 
 /////slices part1
+/////slice_name := []datatype{values}
 package main
 import ("fmt")
 
@@ -224,3 +225,37 @@ func main() {
 }
 
 ///slices part2
+///create a slice from an array
+// var myarray = [length]datatype{values} // An array
+// myslice := myarray[start:end] // A slice made from the array
+package main
+import ("fmt")
+
+func main(){
+	arr1 := [6]int{10, 11, 12, 13, 14,15}
+  myslice := arr1[2:4]
+
+  fmt.Printf("myslice = %v\n", myslice)
+  fmt.Printf("length = %d\n", len(myslice))
+  fmt.Printf("capacity = %d\n", cap(myslice))
+}
+
+///creating a slice from the make function
+
+////slice_name := make([]type, length, capacity)
+package main
+import("fmt")
+
+func main(){
+	myslice1:=make([]int,5,10)
+	fmt.Printf("myslice1=%v\n",myslice1);
+	fmt.Printf("length=%d\n",len(myslice1));
+	fmt.Printf("capacity=%d\n",cap(myslice1));
+
+	//with omitted capacity
+	myslice2:=make([]int,5);
+	fmt.Printf("myslice2=%v\n",myslice2);
+	fmt.Printf("length=%d\n",cap(myslice1));
+	fmt.Printf("capacity = %d\n", cap(myslice2));
+
+}
