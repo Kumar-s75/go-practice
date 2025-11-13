2829278659d02b2f06223c36a71f4e0af021d2a9
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-13 19:16:58 +0530

    multi-switch case

--- new1.go
+++ new1.go
@@ -477,3 +477,23 @@ func main(){
     fmt.Println("Sunday")
   }
 }
+
+///multi case switch example
+
+package main
+import ("fmt")
+ 
+func main(){
+	day:=5
+
+	switch day{
+	case 1,3,5:
+		fmt.Println("Odd weekday")
+    case 2,4:
+		fmt.Println("Even weekday")
+	case 6,7:
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Invalid day of day number")
+	}
+}
