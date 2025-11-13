55a77f6c670485b1ef4df3adfd4880a3fdb93330
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-13 19:35:12 +0530

    range keyword

--- new1.go
+++ new1.go
@@ -520,3 +520,39 @@ func main(){
 		fmt.Println(i)
 	}
 }
+//break statement
+package main
+import ("fmt")
+
+
+func main(){
+ for i:=0;i<5;i++{
+	if i==3{
+		break
+	}
+	fmt.Println(i)
+ }
+}
+
+//nested loops
+package main
+import ("fmt")
+
+func main() {
+  adj := [2]string{"big", "tasty"}
+  fruits := [3]string{"apple", "orange", "banana"}
+  for i:=0; i < len(adj); i++ {
+    for j:=0; j < len(fruits); j++ {
+      fmt.Println(adj[i],fruits[j])
+    }
+  }
+}
+///range keyword
+package main
+import ("fmt")
+func main(){
+	fruits:=[3]string{"apple","orange","banana"}
+	for idx,val:=range fruits{
+		fmt.Printf("%v\t%v\n",idx,val)
+	}
+}
