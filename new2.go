173be7be45a57c9bec29e618d5e54055a1b4fc92
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-14 04:31:04 +0530

    specific elements

--- new2.go
+++ new2.go
@@ -113,4 +113,22 @@ delete(a,"year")
 fmt.Println(a)
 }
 
-////
+////check for specific elements in map
+// val, ok :=map_name[key]
+package main
+import ("fmt")
+
+func main(){
+	var a=map[string]string{"brand":"ford","model":"Mustang","year":"1964","day":""}
+	val1,ok1:=a["brand"]
+	val2,ok2:=a["color"]
+	val3,ok3:=a["day"]
+
+	_,ok4 := a["model"]
+
+	fmt.Println(val1,ok1)
+	fmt.Println(val2,ok2)
+	fmt.Println(val3,ok3)
+	fmt.Println(ok4)
+
+}
