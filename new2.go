d731667223cd1de7a7b0e4b9fe5ab350baba2f04
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-11-14 22:33:02 +0530

    maps last

--- new2.go
+++ new2.go
@@ -160,3 +160,22 @@ func main(){
 		fmt.Printf("%v:%v,",k,v)
 	}
 }
+
+//////iterate over maps in specific order
+package main
+import ("fmt")
+
+func main(){
+  a:=map[string]int{"one":1,"two":2,"three":3,"four":4}
+  var b []string
+  b=append(b,"one","two","three","four")
+
+  for k,v :=range a{
+    fmt.Printf("%v:%v",k,v)
+  }
+  fmt.Println()
+
+  for _,element :=range b{
+    fmt.Printf("%v:%v,",element,a[element])
+  }
+}
